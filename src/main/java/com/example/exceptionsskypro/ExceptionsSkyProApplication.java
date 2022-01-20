397d63d5435ded16f46c5460e77ed28e49dd7153
Kulachenkov <nik.k2710@gmail.com>
2022-01-20 22:36:53 +0300

    Initial commit

---
+++ src/main/java/com/example/exceptionsskypro/ExceptionsSkyProApplication.java
@@ -0,0 +1,13 @@
+package com.example.exceptionsskypro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionsSkyProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExceptionsSkyProApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/exceptionsskypro/ExceptionsSkyProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exceptionsskypro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionsSkyProApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
